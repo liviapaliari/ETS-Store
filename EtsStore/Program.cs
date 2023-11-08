ea027b5c3c2366302fa370a3533a8772e34b8c2b
Livia <liviapadovine@hotmail.com>
2023-11-08 09:16:55 -0300

    First Commit

---
+++ EtsStore/Program.cs
@@ -0,0 +1,23 @@
+﻿using EtsStore;
+
+static void GravarUsandoAdoNet()
+{
+    Produto p = new Produto();
+    p.Nome = "Arduino";
+    p.Categoria = "Componentes Eletrônicos";
+    p.Preco = 59.90;
+
+   
+    var repo = new ProdutoDAO();
+    repo.Adicionar(p);
+
+    foreach (var item in repo.Produtos())
+    {
+        Console.WriteLine(item.Nome);
+    }
+ }
+
+
+
+
+GravarUsandoAdoNet();
